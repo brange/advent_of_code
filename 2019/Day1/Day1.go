fe990877ccee0dd92dee9091e9bbff029011426e
Magnus Brange <magnus@brange.nu>
2019-12-01 10:43:11 +0100

    Update 2019 Day1
    So it prints the answrs to both part one and part two

--- 2019/Day1/Day1.go
+++ 2019/Day1/Day1.go
@@ -1,43 +1,45 @@
 package main
 
 import (
-	"log"
-	"strconv"
-	"strings"
+  "log"
+  "strconv"
+  "strings"
 
-	"github.com/brange/advent_of_code/utils"
+  "github.com/brange/advent_of_code/utils"
 )
 
 func calculateFuel(mass int) int {
-	fuel := mass / 3
-	fuel -= 2
-	if fuel < 0 {
-		fuel = 0
-	}
-	return fuel
+  fuel := mass / 3
+  fuel -= 2
+  if fuel < 0 {
+    fuel = 0
+  }
+  return fuel
 }
 
 func main() {
 
-	var input = utils.FetchInput(2019, 1)
+  var input = utils.FetchInput(2019, 1)
 
-	arr := strings.Split(input, "\n")
+  arr := strings.Split(input, "\n")
 
-	totalFuel := 0
+  totalFuel := 0
+  totalFuelForFuel := 0
 
-	for _, line := range arr {
-		mass, _ := strconv.Atoi(line)
+  for _, line := range arr {
+    mass, _ := strconv.Atoi(line)
 
-		fuel := calculateFuel(mass)
-		fuelForFuel := fuel
+    fuel := calculateFuel(mass)
+    fuelForFuel := fuel
 
-		for ok := true; ok; ok = (fuelForFuel > 0) {
-			fuelForFuel = calculateFuel(fuelForFuel)
-			totalFuel += fuelForFuel
-		}
+    for ok := true; ok; ok = (fuelForFuel > 0) {
+      fuelForFuel = calculateFuel(fuelForFuel)
+      totalFuelForFuel += fuelForFuel
+    }
 
-		totalFuel += fuel
-	}
+    totalFuel += fuel
+  }
 
-	log.Println("totalFueal: ", totalFuel)
+  log.Println("totalFueal: ", totalFuel)
+  log.Println("Total fuel part 2: ", (totalFuel + totalFuelForFuel))
 }
