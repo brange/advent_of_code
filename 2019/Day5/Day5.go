0633ba0537eece6e8b0297ab7b98232077120fc2
Magnus Brange <magnus.brange@telavox.se>
2019-12-05 15:14:49 +0100

    Improved Day5 2019

--- 2019/Day5/Day5.go
+++ 2019/Day5/Day5.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"strconv"
 	"strings"
@@ -17,17 +17,39 @@ func createIntArray(arr []string) []int {
 	return ints
 }
 
+const (
+	ParameterMode int = 0
+	ImmediateMode int = 1
+)
+
+func getParameter(arr []int, index, mode int) int {
+	if mode == ParameterMode {
+		return arr[arr[index]]
+	} else if mode == ImmediateMode {
+		return arr[index]
+	}
+	log.Fatalln("Invalid mode", mode)
+	return -1
+}
+func getPosition(arr []int, index int) int {
+	// 'Parameters that an instruction writes to will never be in immediate mode.'
+	return arr[index]
+}
+
 func runProgram(arr []string, userInput int) int {
 	ints := createIntArray(arr)
 
 	for index := 0; index < len(ints); {
 		input := ints[index]
 
-		var optcode, mode1, mode2, mode3 int
+		var optcode int
+		var mode1, mode2, mode3 int
 		if input >= 1 && input <= 8 {
 			optcode = input
 		} else if input == 99 {
-			log.Println("Breaking, got input 99")
+			if debug {
+				log.Println("Breaking, got input 99")
+			}
 			break
 		} else {
 			optcode = input % 100
@@ -37,39 +59,27 @@ func runProgram(arr []string, userInput int) int {
 		}
 
 		if userInput >= 1 {
-			log.Println("optcode:", optcode, "input", input, "mode1", mode1, "mode2", mode2, "mode3", mode3, "index:", index)
+			if debug {
+				log.Println("optcode:", optcode, "input", input, "mode1", mode1, "mode2", mode2, "mode3", mode3, "index:", index)
+			}
 		}
 		if optcode == 99 {
-			log.Println("Breaking")
+			if debug {
+				log.Println("Breaking")
+			}
 			break
 		}
 
-		var param1 int
-		if mode1 == 1 {
-			param1 = ints[index+1]
-		} else {
-			param1 = ints[ints[index+1]]
-		}
+		param1 := getParameter(ints, index+1, mode1)
 
 		if optcode == 1 || optcode == 2 {
-			val1 := param1
-			var val2, pos int
+			param2 := getParameter(ints, index+2, mode2)
+			pos := getPosition(ints, index+3)
 
-			if mode2 == 1 {
-				val2 = ints[index+2]
-			} else {
-				val2 = ints[ints[index+2]]
-			}
-			if mode3 == 1123123 /*ignored*/ {
-				// Parameters that an instruction writes to will never be in immediate mode.
-				pos = index + 3
-			} else {
-				pos = ints[index+3]
-			}
 			if optcode == 1 {
-				ints[pos] = val1 + val2
+				ints[pos] = param1 + param2
 			} else if optcode == 2 {
-				ints[pos] = val1 * val2
+				ints[pos] = param1 * param2
 			}
 			index += 4
 		} else if optcode == 3 || optcode == 4 {
@@ -79,12 +89,11 @@ func runProgram(arr []string, userInput int) int {
 			 */
 			if optcode == 3 {
 				// Parameters that an instruction writes to will never be in immediate mode.
-				ints[ints[index+1]] = userInput
+				ints[getPosition(ints, index+1)] = userInput
 			} else if optcode == 4 {
 				if param1 != 0 {
 					return param1
 				}
-				//fmt.Printf("%d ", param1)
 			}
 			index += 2
 		} else if optcode == 5 || optcode == 6 {
@@ -92,45 +101,27 @@ func runProgram(arr []string, userInput int) int {
 			 * Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
 			 * Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
 			 */
+			param2 := getParameter(ints, index+2, mode2)
 
-			var param2 int
-			_i := index
-			if mode2 == 1 {
-				param2 = ints[index+2]
-			} else {
-				param2 = ints[ints[index+2]]
-			}
 			if (optcode == 5 && param1 != 0) ||
 				(optcode == 6 && param1 == 0) {
 				index = param2
 			} else {
 				index += 3
 			}
-			log.Println("index, before:", _i, "after:", index)
 		} else if optcode == 7 || optcode == 8 {
 			/*
 			 * Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
 			 * Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
 			 */
-			var param2, param3 int
-			if mode2 == 1 {
-				param2 = ints[index+2]
-			} else {
-				param2 = ints[ints[index+2]]
-			}
-
-			if mode3 == 111 {
-				// Parameters that an instruction writes to will never be in immediate mode.
-				param3 = index + 3
-			} else {
-				param3 = ints[index+3]
-			}
+			param2 := getParameter(ints, index+2, mode2)
+			pos := getPosition(ints, index+3)
 
 			if (optcode == 7 && param1 < param2) ||
 				(optcode == 8 && param1 == param2) {
-				ints[param3] = 1
+				ints[pos] = 1
 			} else {
-				ints[param3] = 0
+				ints[pos] = 0
 			}
 
 			index += 4
@@ -141,13 +132,18 @@ func runProgram(arr []string, userInput int) int {
 
 	}
 
-	fmt.Println("")
 	return -1
 }
+
+var debug bool
 
 func main() {
 	input := utils.FetchInput(2019, 5)
 	arr := strings.Split(input, ",")
+
+	debugPtr := flag.Bool("debug", false, "enable debug")
+	flag.Parse()
+	debug = *debugPtr
 
 	log.Println("Part one")
 	partOne := runProgram(arr, 1)
