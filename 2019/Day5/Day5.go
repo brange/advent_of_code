1aa0e3f60220387159f77b997c88c677182983c0
Magnus Brange <magnus.brange@telavox.se>
2019-12-05 10:22:23 +0100

    Working Day5 2019 step 2

--- 2019/Day5/Day5.go
+++ 2019/Day5/Day5.go
@@ -17,23 +17,31 @@ func createIntArray(arr []string) []int {
 	return ints
 }
 
-func runProgram(arr []string) int {
+func runProgram(arr []string, userInput int) int {
 	ints := createIntArray(arr)
 
 	for index := 0; index < len(ints); {
 		input := ints[index]
-		if input == 99 {
-			//log.Println("Breaking")
-			break
-		}
+
 		var optcode, mode1, mode2, mode3 int
-		if input >= 1 && input <= 4 {
+		if input >= 1 && input <= 8 {
 			optcode = input
+		} else if input == 99 {
+			log.Println("Breaking, got input 99")
+			break
 		} else {
 			optcode = input % 100
 			mode1 = ((input - optcode) % 1000) / 100
 			mode2 = ((input - mode1 - optcode) % 10000) / 1000
 			mode3 = ((input - mode2 - mode1 - optcode) % 100000) / 10000
+		}
+
+		if userInput >= 1 {
+			log.Println("optcode:", optcode, "input", input, "mode1", mode1, "mode2", mode2, "mode3", mode3, "index:", index)
+		}
+		if optcode == 99 {
+			log.Println("Breaking")
+			break
 		}
 
 		var param1 int
@@ -43,7 +51,6 @@ func runProgram(arr []string) int {
 			param1 = ints[ints[index+1]]
 		}
 
-		log.Println("optcode:", optcode, "input", input, "mode1", mode1, "mode2", mode2, "mode3", mode3)
 		if optcode == 1 || optcode == 2 {
 			val1 := param1
 			var val2, pos int
@@ -53,7 +60,7 @@ func runProgram(arr []string) int {
 			} else {
 				val2 = ints[ints[index+2]]
 			}
-			if mode3 == 1 {
+			if mode3 == 1123123 /*ignored*/ {
 				// Parameters that an instruction writes to will never be in immediate mode.
 				pos = index + 3
 			} else {
@@ -66,15 +73,28 @@ func runProgram(arr []string) int {
 			}
 			index += 4
 		} else if optcode == 3 || optcode == 4 {
+			/*
+			 * Opcode 3 takes a single integer as input and saves it to the address given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50.
+			 * Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50.
+			 */
 			if optcode == 3 {
 				// Parameters that an instruction writes to will never be in immediate mode.
-				ints[ints[index+1]] = 1
+				ints[ints[index+1]] = userInput
 			} else if optcode == 4 {
-				fmt.Printf("%d", param1)
+				if param1 != 0 {
+					return param1
+				}
+				//fmt.Printf("%d ", param1)
 			}
 			index += 2
-		} /*else if optcode == 5 || optcode == 6 {
+		} else if optcode == 5 || optcode == 6 {
+			/*
+			 * Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+			 * Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+			 */
+
 			var param2 int
+			_i := index
 			if mode2 == 1 {
 				param2 = ints[index+2]
 			} else {
@@ -84,20 +104,26 @@ func runProgram(arr []string) int {
 				(optcode == 6 && param1 == 0) {
 				index = param2
 			} else {
-				index += 2
+				index += 3
 			}
+			log.Println("index, before:", _i, "after:", index)
 		} else if optcode == 7 || optcode == 8 {
+			/*
+			 * Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+			 * Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+			 */
 			var param2, param3 int
 			if mode2 == 1 {
 				param2 = ints[index+2]
 			} else {
 				param2 = ints[ints[index+2]]
 			}
-			if mode3 == 1111 {
+
+			if mode3 == 111 {
 				// Parameters that an instruction writes to will never be in immediate mode.
-				param3 = ints[index+3]
+				param3 = index + 3
 			} else {
-				param3 = ints[ints[index+3]]
+				param3 = ints[index+3]
 			}
 
 			if (optcode == 7 && param1 < param2) ||
@@ -107,14 +133,8 @@ func runProgram(arr []string) int {
 				ints[param3] = 0
 			}
 
+			index += 4
 		}
-		*/
-		/*
-			Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-			Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-			Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-			Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-		*/
 		if optcode == 0 {
 			break
 		}
@@ -122,13 +142,20 @@ func runProgram(arr []string) int {
 	}
 
 	fmt.Println("")
-	return ints[0]
+	return -1
 }
 
 func main() {
-	arr := strings.Split(utils.FetchInput(2019, 5), ",")
+	input := utils.FetchInput(2019, 5)
+	arr := strings.Split(input, ",")
 
-	partOne := runProgram(arr)
+	log.Println("Part one")
+	partOne := runProgram(arr, 1)
+	log.Println("------------")
+	log.Println("Part two")
+	partTwo := runProgram(arr, 5)
 
-	log.Println("Part one: ", partOne)
+	log.Println("Part one answer:", partOne)
+	log.Println("Part two answer:", partTwo)
+
 }
